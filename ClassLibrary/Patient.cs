56e70b7ce1419e248e839bf1d8a067a5612574a1
danieelfcr <danielfcab2002@gmail.com>
2022-03-23 00:02:08 -0600

    Patient Class created and props added

---
+++ ClassLibrary/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Patient
+    {
+        public string Names { get; set; }
+        public string LastNames { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public int Specialization { get; set; }
+        public string EntryMethod { get; set; }
+
+    }
+}
